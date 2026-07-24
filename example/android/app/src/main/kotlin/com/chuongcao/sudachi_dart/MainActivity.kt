8a469d1a32f96ca7977f40ec6db43ef289f6231c
chuongcao <chuongch@azg.jp>
2026-07-24 11:50:25 +0700

    Init

---
+++ example/android/app/src/main/kotlin/com/chuongcao/sudachi_dart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chuongcao.sudachi_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/sudachi_dart_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+import 'package:sudachi_dart/sudachi_dart.dart';
+
+void main() {
+  test('invoke native function', () {
+    expect(sum(24, 18), 42);
+  });
+
+  test('invoke async native callback', () async {
+    expect(await sumAsync(24, 18), 42);
+  });
+}
